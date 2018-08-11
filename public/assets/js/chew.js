b02f2dff23133f773d9d6e63eb9b21528fb2a8e4
philipyoo <philip.yoo@scality.com>
2018-08-11 15:07:58 -0700

    connect fb db with plotly

--- public/assets/js/chew.js
+++ public/assets/js/chew.js
@@ -8,25 +8,34 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+// let url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+// let xl = []
+// let yl = []
+
 database.ref().on("value", function(snapshot) {
-    console.log(snapshot.val().data[0][1]);
-    console.log("hello");
-});
+    console.log('EVENT: value')
+    // const dates = snapshot.val().data[i][0];
+    const dates = snapshot.val().data.map(i => i[0]);
+    // const vals = snapshot.val().data[0][1];
+    const vals = snapshot.val().data.map(i => i[1]);
 
+    // xl.push(singleDate);
+    // yl.push(singleVal);
+    // console.log(snapshot.val().data[0][1]);
 
-let url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
-let xl = []
-let yl = []
-
-Plotly.d3.json(url, function (figure) {
-    let data = figure.data
-    for (var i = 0; i < data.length; i++) {
-        xl.push(data[i][0])
-        yl.push(data[i][1])
-    }
     let trace = {
-        x: xl,
-        y: yl
+        x: dates,
+        y: vals,
     }
+
     Plotly.plot(document.getElementById("graph"), [trace]);
-})
+    // Plotly.newPlot("div#graph", trace);
+        // let data = figure.data
+        // for (var i = 0; i < data.length; i++) {
+        //     xl.push(data[i][0])
+        //     yl.push(data[i][1])
+        // }
+
+        // Plotly.plot(document.getElementById("graph"), [trace]);
+    // });
+});
