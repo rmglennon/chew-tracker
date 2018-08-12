40e8d48c6a5e243586095f6bdba97dee3bad3b0e
Rhonda Glennon <rhondaglennon@gmail.com>
2018-08-11 23:50:16 -0700

    add graphs, add pacman, update styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,33 +8,91 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-database.ref().on("value", function(snapshot) {
-   // console.log('EVENT: value')
-    console.log(snapshot.val())
+database.ref().on("value", function (snapshot) {
+
     var myData = snapshot.val();
-    
+
     var myTimes = []
     for (var key in myData) {
         if (myData.hasOwnProperty(key)) {
-         //   console.log(key + " -> " + myData[key]);
             for (var key2 in myData[key]) {
-              //  console.log(myData[key][key2])
                 myTimes.push(myData[key][key2])
             }
         }
     }
-    console.log(myTimes);
-    //const dates = snapshot.val().data[i][0];
 
     const dates = myTimes.map(i => new Date(i));
 
-    console.log(dates);
-    var trace1 = {
-        x: myTimes,
-        type: 'scatter'
-      };
-      
-      var data = [trace1];
-      
-      Plotly.newPlot("graph", data);
+    $('#animated-pacman').addClass('animated');
+    setTimeout(function () { $('#animated-pacman').removeClass('animated') }, 2000);
+
+    makeScatter(dates);
+    makeBarChart(dates);
+    makeBubbleChart(dates);
+
 });
+
+function makeBarChart(dates) {
+
+    var trace = {
+        x: dates,
+        type: 'histogram',
+    };
+
+    var data = [trace];
+    Plotly.newPlot('histogram', data);
+}
+
+function makeScatter(dates) {
+    var trace = {
+        x: dates,
+        type: 'scatter',
+        mode: 'markers'
+    };
+
+    var layout = {
+        title: 'Your recent chews',
+        xaxis: {
+            title: 'Time',
+            titlefont: {
+                family: "Comic Sans MS, cursive, sans-serif",
+                size: 18,
+                color: "#ff66ff"
+            }
+        },
+        yaxis: {
+            title: 'Number of chews',
+            titlefont: {
+                family: "Comic Sans MS, cursive, sans-serif",
+                size: 18,
+                color: '#ff66ff'
+            }
+        }
+    };
+
+    var data = [trace];
+
+    Plotly.newPlot("line-graph", data, layout);
+}
+
+function makeBubbleChart(dates) {
+    var trace = {
+        x: dates,
+        mode: 'markers',
+        marker: {
+            color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)', 'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+            opacity: [1, 0.8, 0.6, 0.4],
+            size: [40, 60, 80, 100]
+        }
+    };
+
+    var data = [trace];
+
+    var layout = {
+        title: 'With bubbles',
+        height: 400,
+        width: 480
+    };
+
+    Plotly.newPlot('bubble-chart', data, layout);
+}
