6a6f148622a48075c15cc516936e37074cd8407a
Rhonda Glennon <rhondaglennon@gmail.com>
2018-08-11 17:23:12 -0700

    loop through firebase data

--- public/assets/js/chew.js
+++ public/assets/js/chew.js
@@ -13,22 +13,36 @@ var database = firebase.database();
 // let yl = []
 
 database.ref().on("value", function(snapshot) {
-    console.log('EVENT: value')
-    // const dates = snapshot.val().data[i][0];
-    const dates = snapshot.val().data.map(i => i[0]);
-    // const vals = snapshot.val().data[0][1];
-    const vals = snapshot.val().data.map(i => i[1]);
+   // console.log('EVENT: value')
+    console.log(snapshot.val())
+    var myData = snapshot.val();
+    
+    var myTimes = []
+    for (var key in myData) {
+        if (myData.hasOwnProperty(key)) {
+         //   console.log(key + " -> " + myData[key]);
+            for (var key2 in myData[key]) {
+              //  console.log(myData[key][key2])
+                myTimes.push(myData[key][key2])
+            }
+        }
+    }
+    console.log(myTimes);
+    //const dates = snapshot.val().data[i][0];
+    // const dates = snapshot.val().map(i => i[0]);
+    // // const vals = snapshot.val().data[0][1];
+    // const vals = snapshot.val().map(i => i[1]);
 
     // xl.push(singleDate);
     // yl.push(singleVal);
     // console.log(snapshot.val().data[0][1]);
 
-    let trace = {
-        x: dates,
-        y: vals,
-    }
+    // let trace = {
+    //     x: dates,
+    //     y: vals,
+    // }
 
-    Plotly.plot(document.getElementById("graph"), [trace]);
+    // Plotly.plot(document.getElementById("graph"), [trace]);
     // Plotly.newPlot("div#graph", trace);
         // let data = figure.data
         // for (var i = 0; i < data.length; i++) {
