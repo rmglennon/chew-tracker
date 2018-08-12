aed9c479b7766f8b32b2d6df061f0d51976e0fab
Rhonda Glennon <rhondaglennon@gmail.com>
2018-08-12 12:42:06 -0700

    update css for boxes

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,7 +26,7 @@ database.ref().on("value", function (snapshot) {
     const dates = myTimes.map(i => new Date(i));
 
     $('#animated-pacman').addClass('animated');
-    setTimeout(function () { $('#animated-pacman').removeClass('animated') }, 2000);
+    setTimeout(function () { $('#animated-pacman').removeClass('animated') }, 100);
 
     makeScatter(dates);
     makeBarChart(dates);
@@ -164,63 +164,70 @@ function makeGaugeChart(dates) {
 
     // Trig to calc meter point
     const degrees = 180 - level,
-         radius = .5;
+        radius = .5;
     const radians = degrees * Math.PI / 180;
     const x = radius * Math.cos(radians);
     const y = radius * Math.sin(radians);
 
     // Path: may have to change to create a better triangle
     const mainPath = 'M -.0 -0.025 L .0 0.025 L ',
-         pathX = String(x),
-         space = ' ',
-         pathY = String(y),
-         pathEnd = ' Z';
-    const path = mainPath.concat(pathX,space, pathY,pathEnd);
+        pathX = String(x),
+        space = ' ',
+        pathY = String(y),
+        pathEnd = ' Z';
+    const path = mainPath.concat(pathX, space, pathY, pathEnd);
 
-    const data = [{ type: 'scatter',
-       x: [0], y:[0],
-        marker: {size: 28, color:'850000'},
+    const data = [{
+        type: 'scatter',
+        x: [0], y: [0],
+        marker: { size: 28, color: '850000' },
         showlegend: false,
         name: 'speed',
         text: level,
-        hoverinfo: 'text+name'},
-      { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
-      rotation: 90,
-      text: ['TOO FAST!', 'Pretty Fast', 'Fast', 'Average',
-             'Slow', 'Super Slow', ''],
-      textinfo: 'text',
-      textposition:'inside',
-      marker: {
-          colors: [
-              'rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
-              'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
-              'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
-              'rgba(255, 255, 255, 0)',
-          ]
-      },
-      labels: ['240+', '193-240', '145-192', '97-144', '49-96', '0-48', ''],
-      hoverinfo: 'label',
-      hole: .5,
-      type: 'pie',
-      showlegend: false
+        hoverinfo: 'text+name'
+    },
+    {
+        values: [50 / 6, 50 / 6, 50 / 6, 50 / 6, 50 / 6, 50 / 6, 50],
+        rotation: 90,
+        text: ['TOO FAST!', 'Pretty Fast', 'Fast', 'Average',
+            'Slow', 'Super Slow', ''],
+        textinfo: 'text',
+        textposition: 'inside',
+        marker: {
+            colors: [
+                'rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                'rgba(255, 255, 255, 0)',
+            ]
+        },
+        labels: ['240+', '193-240', '145-192', '97-144', '49-96', '0-48', ''],
+        hoverinfo: 'label',
+        hole: .5,
+        type: 'pie',
+        showlegend: false
     }];
 
     const layout = {
-      shapes:[{
-          type: 'path',
-          path: path,
-          fillcolor: '850000',
-          line: {
-            color: '850000'
-          }
+        shapes: [{
+            type: 'path',
+            path: path,
+            fillcolor: '850000',
+            line: {
+                color: '850000'
+            }
         }],
-      title: 'Chew Frequency',
-      height: 500,
-      width: 500,
-      xaxis: {zeroline:false, showticklabels:false,
-                 showgrid: false, range: [-1, 1]},
-      yaxis: {zeroline:false, showticklabels:false,
-                 showgrid: false, range: [-1, 1]}
+        title: 'Chew Frequency',
+        // height: 300,
+        // width: 500,
+        xaxis: {
+            zeroline: false, showticklabels: false,
+            showgrid: false, range: [-1, 1]
+        },
+        yaxis: {
+            zeroline: false, showticklabels: false,
+            showgrid: false, range: [-1, 1]
+        }
     };
 
     Plotly.react('gauge-chart', data, layout);
